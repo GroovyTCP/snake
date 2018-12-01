003e5d2c0924bfe2a07b82490f5aea598d4ebe86
Tomas Handley <tomashandley@hotmail.com>
2018-12-01 04:06:11 -0300

    ping pong snake
    se puede actualizar la serpiente, y se devuelven las demas serpientes

---
+++ SnakeTCP/src/viborita/conexion/UpdateVivora.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package viborita.conexion;
+
+import viborita.entidades.Vibora;
+
+/**
+ * @author mcurrao
+ *
+ */
+public class UpdateVivora {
+
+	private String username;
+	private Vibora vibora;
+	private String sala;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public Vibora getVibora() {
+		return vibora;
+	}
+
+	public void setVibora(Vibora vibora) {
+		this.vibora = vibora;
+	}
+
+	public String getSala() {
+		return sala;
+	}
+
+	public void setSala(String sala) {
+		this.sala = sala;
+	}
+
+}
---
+++ SnakeTCP/src/viborita/servidor/SnakePit.java
@@ -0,0 +1,41 @@
+package viborita.servidor;
+
+import java.util.Map;
+
+import viborita.conexion.UpdateVivora;
+import viborita.entidades.Vibora;
+
+public class SnakePit {
+
+	private static SnakePit snakePit;
+
+	private Map<String, Map<String, Vibora>> viborasPorSalaPorUsuario;
+
+	public static synchronized SnakePit getInstance() {
+		if (snakePit == null)
+			snakePit = new SnakePit();
+		return snakePit;
+	}
+
+	public Map<String, Vibora> revolver(UpdateVivora update) {
+		if (update == null || update.getSala() == null)
+			throw new RuntimeException("Debe ingresar a una sala para poder jugar");
+
+		Map<String, Vibora> viborasEnLaSala = viborasPorSalaPorUsuario.get(update.getSala());
+
+		// si la vibora es null, entonces solo devuelvo pero no updateo
+		if (update.getVibora() != null) {
+			// actualiza el estado de la vibora en base a lo que nos avise el
+			// usuario, y devuelve todas las vivoras que haya en el pozo para la
+			// sala actual
+			String usuarioAsociado = update.getUsername();
+			if (usuarioAsociado == null)
+				throw new RuntimeException("Una serpiente salvaje aparece");
+
+			viborasEnLaSala.put(usuarioAsociado, update.getVibora());
+		}
+
+		return viborasEnLaSala;
+	}
+
+}
